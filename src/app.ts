b48b055b75c911956fcc19c0b534592f412d7de5
REVOFT PVT LTD <admin@revoft.com>
2025-10-18 10:31:57 +0000

    Path update to use versioning with api gateway

--- src/app.ts
+++ src/app.ts
@@ -54,7 +54,7 @@ if (config.env === 'production') {
 }
 
 // v1 api routes
-app.use('/v1/lsa',routes);
+app.use('/v1',routes);
 
 // send back a 404 error for any unknown api request
 app.use((_req, _res, next) => {
